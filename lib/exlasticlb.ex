f301c2edf3d915f0f11ca21e98878713bd139238
Ubuntu <ubuntu@ip-172-31-21-244.us-west-2.compute.internal>
2016-06-30 14:54:55 +0000

    Slightly better way to call test function

--- lib/exlasticlb.ex
+++ lib/exlasticlb.ex
@@ -12,11 +12,12 @@ defmodule AWS do
     stats = :erlcloud_mon.get_metric_statistics(
       'CPUUtilization',
       {{2016, 06, 29},{0, 0, 0}},
-      {{2016, 06, 30},{0, 0, 0}},
+      {{2016, 06, 29},{0, 30, 0}},
       'i-1902ba9f'
     )
-    #IO.puts Kernel.inspect(stats, pretty: true)
-    
+    IO.puts Kernel.inspect(stats, pretty: true)
+    stats
+
     #IO.puts 1+1
   end
 
